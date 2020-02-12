f5dcf5e7d5d3c5957f3ffd634aaee1325219d286
nooboolean <nooboolean@i.softbank.jp>
2020-02-13 02:42:57 +0900

    ユーザー作成&ログイン実装&tokenId取得

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: process.env.FIRE_BASE_API_KEY,
+  authDomain: process.env.FIRE_BASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIRE_BASE_DATABASE_URL,
+  projectId: process.env.FIRE_BASE_PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: process.env.FIRE_BASE_MESSAGEING_SENDERID,
+  appId: process.env.FIRE_BASE_APP_ID
+}
+
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
